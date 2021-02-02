0cfb42f70dd0c63d79c7d4c7dafd9bee69cc7c7e
mcox-dev <miguelcox.dev@gmail.com>
2021-02-02 09:52:34 -0500

    Adding custom user repository with migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'sub',
     ];
 
     /**
@@ -28,7 +28,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
         'remember_token',
     ];
 
